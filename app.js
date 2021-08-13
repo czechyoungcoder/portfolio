25805f4f5fd7e75370131a8b906ec0b3af68c2fa
Jakub Podskalský <p0dsk4lsky@gmail.com>
2021-08-13 20:46:44 +0200

    starting files setup

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express'),
+      cookieParser = require('cookie-parser'),
+      smws = require("smws"),
+      app = express();
+
+app.set('view engine', 'Eta');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(__dirname + '/public'));
+
+
+
+
+// SMWS CONFIG
+smws.config({
+  languages: ['cs','en'],
+  defaultLang: 'en'
+});
+
+app.post('/:lang/language', (req,res)=>{
+  smws.switcher(req,res);
+});
+
+app.get('/', function (req, res) {
+
+  smws.run(req, res, {
+      page: 'index'
+  });
+
+});
+
+app.get('/:lang', function (req, res) {
+
+  smws.run(req, res, {
+      page: 'index'
+  });
+
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server running on port 3000');
+});
