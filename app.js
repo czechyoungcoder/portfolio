a9c660e4bbbc4a4be52c8f01ba2f4d889be47050
Jakub Podskalský <p0dsk4lsky@gmail.com>
2021-10-02 22:58:47 +0200

    okay lets try sth...

--- app.js
+++ app.js
@@ -15,54 +15,54 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(__dirname + '/public'));
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com", //replace with your email provider
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASS,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com", //replace with your email provider
+//   port: 465,
+//   secure: true,
+//   auth: {
+//     user: process.env.EMAIL,
+//     pass: process.env.PASS,
+//   },
+// });
 
-app.post("/send", (req, res) => {
-  //1.
-  let form = new multiparty.Form();
-  let data = {};
-  form.parse(req, function (err, fields) {
-    console.log(fields);
-    Object.keys(fields).forEach(function (property) {
-      data[property] = fields[property].toString();
-    });
+// app.post("/send", (req, res) => {
+//   //1.
+//   let form = new multiparty.Form();
+//   let data = {};
+//   form.parse(req, function (err, fields) {
+//     console.log(fields);
+//     Object.keys(fields).forEach(function (property) {
+//       data[property] = fields[property].toString();
+//     });
 
-    //2. You can configure the object however you want
-    const mail = {
-      from: data.name,
-      to: process.env.EMAIL,
-      subject: data.subject,
-      text: `${data.name} <${data.email}> \n${data.message}`,
-    };
+//     //2. You can configure the object however you want
+//     const mail = {
+//       from: data.name,
+//       to: process.env.EMAIL,
+//       subject: data.subject,
+//       text: `${data.name} <${data.email}> \n${data.message}`,
+//     };
 
-    //3.
-    transporter.sendMail(mail, (err, data) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send("Something went wrong.");
-      } else {
-        res.status(200).send("Email successfully sent to recipient!");
-      }
-    });
-  });
-});
+//     //3.
+//     transporter.sendMail(mail, (err, data) => {
+//       if (err) {
+//         console.log(err);
+//         res.status(500).send("Something went wrong.");
+//       } else {
+//         res.status(200).send("Email successfully sent to recipient!");
+//       }
+//     });
+//   });
+// });
 
-// verify connection configuration
-transporter.verify(function (error, success) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Server is ready to take our messages");
-  }
-});
+// // verify connection configuration
+// transporter.verify(function (error, success) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("Server is ready to take our messages");
+//   }
+// });
 
 // SMWS CONFIG
 smws.config({
