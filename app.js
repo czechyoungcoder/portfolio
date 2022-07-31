82058f24aaeed4aa4975fc90f98a95dd5351f6a5
Jakub Podskalský <p0dsk4lsky@gmail.com>
2022-07-31 19:14:37 +0200

    enabling cors

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ require("dotenv").config();
 
 app.set('view engine', 'eta');
 
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
